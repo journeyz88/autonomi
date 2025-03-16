66718ea191307c8884bf25daea483e73bcdf41db
journeyz88 <zhulin880117@gmail.com>
2025-03-17 01:09:12 +0800

    up

--- ant-node/src/bin/antnode/main.rs
+++ ant-node/src/bin/antnode/main.rs
@@ -540,7 +540,8 @@ fn init_logging(opt: &Opt, peer_id: PeerId) -> Result<(String, ReloadHandle, Opt
     // };
     // 强制所有日志输出到标准输出
     // 使用内存中的虚拟路径避免文件系统操作
-    let output_dest = LogOutputDest::Path(PathBuf::from("/dev/null").join("antnode.log")); // 虚拟文件路径
+    // let output_dest = LogOutputDest::Path(PathBuf::from("/dev/null").join("antnode.log")); // 虚拟文件路径
+    let output_dest = LogOutputDest::Stdout;
 
     #[cfg(not(feature = "otlp"))]
     let (reload_handle, log_appender_guard) = {
